825e4e8aceccadde544fac17f17c3023f583a768
dmytroPustovit <pustic@gmail.com>
2017-05-01 12:26:46 +0300

    input issue resolved

--- src/js/defaultCtrl.js
+++ src/js/defaultCtrl.js
@@ -6,33 +6,40 @@ AngApp.controller("defaultCtrl", function($scope,$window,$http) {
 		})
 	}
 	$scope.AddItem = function(){
-		$scope.items.push({
-			name: $scope.item.name,
-			comments: [], 
-			number: $scope.items.length,
-			id: Date.now()
-		});
-		$scope.item.name = "";
-		$window.localStorage.setItem(storageName, JSON.stringify($scope.items));	
+		if(~$scope.item.name.search(/\S/)){
+			$scope.items.push({
+				name: $scope.item.name,
+				comments: [], 
+				number: $scope.items.length,
+				id: Date.now()
+			});
+			$window.localStorage.setItem(storageName, JSON.stringify($scope.items));	
+		}
+		$scope.item.name = null;		
 	}
 	$scope.DeleteItem = function(item){
 		number=item.number;
 		$scope.items.splice(number,1);
 		$scope.items.forEach(function(v,i,a)
-			{
-				if(i>=number){
-					a[i].number=v.number-1;
-				}
-			});
+		{
+			if(i>=number){
+				a[i].number=v.number-1;
+			}
+		});
 		if($scope.active==number+1){
 			$scope.active=-1;
 		}
 		$window.localStorage.setItem(storageName, JSON.stringify($scope.items));
 	}
 	$scope.AddCommment = function(){
-		$scope.items[$scope.active-1].comments.push($scope.comment);
-		$window.localStorage.setItem(storageName, JSON.stringify($scope.items));
-		$scope.comment = "";		
+		if(~$scope.comment.search(/\S/)){
+			$scope.items[$scope.active-1].comments.push({
+				text: $scope.comment,
+				id: $scope.items[$scope.active-1].comments.length+1 
+			});
+			$window.localStorage.setItem(storageName, JSON.stringify($scope.items));
+		}
+		$scope.comment = null;		
 	}
 	var storageName = "empeek-items";
 	$scope.items = JSON.parse($window.localStorage.getItem(storageName));
